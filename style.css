27239fd2c4f99b71b4ca4944992edbac56b8b829
Sujeet Kumar <38582345+sujeet712@users.noreply.github.com>
2025-11-06 17:00:58 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,19 @@
+function fun2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97 ";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function fun1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function fun3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function fun4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+}
